012383337710b41be261124a0fb8e843437e72fa
Taisoul <64573602+Taisoul@users.noreply.github.com>
2020-05-12 14:08:50 +0700

    Update manage.php

--- dct2/application/views/drawing/manage.php
+++ dct2/application/views/drawing/manage.php
@@ -46,7 +46,7 @@
                 echo"</td>";
                 echo "<td>$r->dcn_no</td>";
                 echo "<td>$r->version</td>";
-                echo "<td>$r->status</td>";
+                
 
                 
                 if($r->enable!=1 ){?>
@@ -73,6 +73,9 @@
 
                 <?php 
                 echo "<a type='button' href='".base_url()."drawing/deletedrawing/".$r->d_id."' onclick='return confirm(\"Confirm Delete Item\")' ><i class='btn-default btn-sm fa fa-trash'></i></a></td>";
+                ?>
+                <td class="text-center"><b><font color="green">active</font></b></td>
+                <?php
             echo "</tr>";
         }
     ?>
