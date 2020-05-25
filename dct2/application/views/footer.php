39d170763bbf3100a751c572f06a53c112fff254
nongpavcd <nongpavcd@hotmail.co.th>
2020-05-25 13:34:52 +0700

    dashboard

--- dct2/application/views/footer.php
+++ dct2/application/views/footer.php
@@ -1,6 +1,7 @@
 
       </div>
     </div>
+    
     <script src="<?php echo base_url().'assets/js/vendor.min.js'; ?>"></script>
     <script src="<?php echo base_url().'assets/js/elephant.min.js'; ?>"></script>
     <script src="<?php echo base_url().'assets/js/application.min.js'; ?>"></script>
