fa136aa7bcac6a27bd05866c0503439586e0ffc9
msuphako <55439852+msuphako@users.noreply.github.com>
2020-07-01 13:56:21 +0700

    Update Drawing.php

--- dct2/application/controllers/Drawing.php
+++ dct2/application/controllers/Drawing.php
@@ -292,6 +292,7 @@ class Drawing extends CI_Controller {
         $file =  $this->input->post('file');
         $path = $this->input->post('path');
         $open = ("$path$file");
+
         exec($open);
         if($open){
             echo '<script language="javascript">';
@@ -311,6 +312,8 @@ class Drawing extends CI_Controller {
         $path = $this->input->post('path');
         $open = ("$path$file");
         exec($open);
+        echo $open;
+        exit;
         if($open){
             echo '<script language="javascript">';
                 echo 'history.go(-1);';
