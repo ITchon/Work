731128fa118a19aafcee8b6ec62e0abf38b1b430
Taisoul <64573602+Taisoul@users.noreply.github.com>
2020-08-20 09:48:15 +0700

    Model drawing,dcn

--- dct/application/controllers/Dcn.php
+++ dct/application/controllers/Dcn.php
@@ -11,7 +11,7 @@ class Dcn extends CI_Controller {
         $this->load->helper('form');
         $this->load->database(); 
         $this->load->model('model');     
-        $this->load->model('dcn');     
+        $this->load->model('model_dcn');
         $this->model->load_menu();
         $this->model->button_show($this->session->userdata('su_id'),8);
     }
@@ -28,11 +28,7 @@ class Dcn extends CI_Controller {
             $dcn_id = $this->uri->segment('3');
             $text = "and dc.dcn_id = $dcn_id ";
         }
-        $sql =  "SELECT dc.dcn_id, dc.dcn_no,dc.file_name as dcn_file,dc.path_file as dcn_path,dc.file_code as dcn_code,dc.enable
-          from dcn as dc
-          where dc.delete_flag != 0 $text";
-            $query = $this->db->query($sql); 
-        $data['result'] = $query->result(); 
+        $data['result'] = $this->model_dcn->get_dcn_byid($text); 
         $this->load->view('dcn/show',$data);//bring $data to user_data 
         $this->load->view('footer');
 
@@ -62,7 +58,7 @@ class Dcn extends CI_Controller {
         $dcn_no =  $this->input->post('dcn_no');
         $path =  $this->input->post('path');
         $file =  $this->input->post('file_name');
-        $result = $this->model->insert_dcn($dcn_no,$path,$file);
+        $result = $this->model_dcn->insert_dcn($dcn_no,$path,$file);
         
 
         if($result == true){
@@ -91,7 +87,7 @@ class Dcn extends CI_Controller {
         $this->model->CheckPermission($this->session->userdata('su_id'));
         $this->model->CheckPermissionGroup($this->session->userdata('sug_id'));
         $id =  $this->uri->segment('3');
-        $this->model->delete_dcn($id);
+        $this->model_dcn->delete_dcn($id);
         redirect('dcn/manage/'.$id.'','refresh');
     }
 
@@ -122,7 +118,7 @@ class Dcn extends CI_Controller {
         if($file_name==null){
             $file_name =  $this->input->post('file_name_old');
         }
-       $this->model->save_dcn($dcn_id,$dcn_no,$path_file,$file_name);
+       $this->model_dcn->save_dcn($dcn_id,$dcn_no,$path_file,$file_name);
 
         redirect('dcn/manage/'.$dcn_id.'','refresh');
   
@@ -133,7 +129,7 @@ class Dcn extends CI_Controller {
         $this->model->CheckPermission($this->session->userdata('su_id'));
         $this->model->CheckPermissionGroup($this->session->userdata('sug_id'));
 
-        $result = $this->model->enableDcn($uid);
+        $result = $this->model_dcn->enableDcn($uid);
 
         if($result!=FALSE){
             redirect('dcn/manage/'.$uid.'','refresh');
@@ -149,7 +145,7 @@ class Dcn extends CI_Controller {
         $this->model->CheckPermission($this->session->userdata('su_id'));
         $this->model->CheckPermissionGroup($this->session->userdata('sug_id'));
 
-        $result = $this->model->disableDcn($uid);
+        $result = $this->model_dcn->disableDcn($uid);
 
         if($result!=FALSE){
             redirect('dcn/manage/'.$uid.'','refresh');
@@ -164,7 +160,7 @@ class Dcn extends CI_Controller {
     {       
 
         $tf_id =  $this->input->post('tf_id');
-        $folder = $this->model->checkfolder($tf_id);
+        $folder = $this->model_drawing->checkfolder($tf_id);
         $config['upload_path']           = './uploads/'.$folder.'/';
         $config['allowed_types']        = '*';
         $config['max_size']        = '0';
@@ -202,7 +198,7 @@ class Dcn extends CI_Controller {
                 $uploaded = $this->upload->data();
     $code = array('filename'  => $uploaded['file_name']);
     foreach ($code as $c) {
-        $result = $this->model->insert_dcn($dcn_no,$path,$file,$c,$tf_id);
+        $result = $this->model_dcn->insert_dcn($dcn_no,$path,$file,$c,$tf_id);
     }
             $this->session->set_flashdata('success','<div class="alert alert-success hide-it">  
           <span> เพิ่มข้อมูลเรียบร้อยเเล้ว </span>
