219581f457f4e85aded6ebadc4374ad5fd3655c4
msuphako <55439852+msuphako@users.noreply.github.com>
2020-08-27 13:42:39 +0700

    54

--- dct/application/models/model_drawing.php
+++ dct/application/models/model_drawing.php
@@ -338,13 +338,14 @@ $path_file = quotemeta($path_file);
       }
       $sql =  "SELECT d.d_id,p.p_id,pd.p_id as pd_pid,pd.d_id as pd_did,d.d_no,d.d_name,c.cus_id,c.cus_name,
        d.dcn_id, dc.dcn_no, d.enable, d.file_name, d.version, d.path_file, p.p_no,dc.file_name as dcn_file,
-       dc.path_file as dcn_path,d.file_code,dc.file_code as dcn_code ,d.f_id,d.pos,f.folder_name
+       dc.path_file as dcn_path,d.file_code,dc.file_code as dcn_code ,d.f_id,d.pos,f.folder_name,fg.foldergroup_name
       from drawing as d
         left join part_drawing as pd on pd.d_id = d.d_id
         left join customers as c on c.cus_id = d.cus_id
         left join dcn as dc on dc.dcn_id = d.dcn_id
         left join part as p on p.p_id = pd.p_id
         inner join folder f on f.f_id = d.f_id
+        inner join folder_group fg on fg.fg_id = f.fg_id
         where d.delete_flag != 0 AND d.f_id IN ($folder) AND (d.d_no LIKE '%$s_dno%' OR d.d_name LIKE '%$s_name%' OR p.p_no LIKE '%$s_pno%')";
 
       $query = $this->db->query($sql);
@@ -357,13 +358,15 @@ $path_file = quotemeta($path_file);
   {
     $sql =  "SELECT d.d_id,p.p_id,pd.p_id as pd_pid,pd.d_id as pd_did,d.d_no,d.d_name,c.cus_id,c.cus_name,
      d.dcn_id, dc.dcn_no, d.enable, d.file_name, d.version, d.path_file, p.p_no,dc.file_name as dcn_file,
-     dc.path_file as dcn_path,d.file_code,dc.file_code as dcn_code, d.f_id,d.pos,f.folder_name
+     dc.path_file as dcn_path,d.file_code,dc.file_code as dcn_code, d.f_id,d.pos,f.folder_name,fg.foldergroup_name
           from drawing as d
             left join part_drawing as pd on pd.d_id = d.d_id
             left join customers as c on c.cus_id = d.cus_id
             left join dcn as dc on dc.dcn_id = d.dcn_id
             left join part as p on p.p_id = pd.p_id
             inner join folder f on f.f_id = d.f_id
+            inner join folder_group fg on fg.fg_id = f.fg_id
+
           where d.delete_flag != 0";
        $query = $this->db->query($sql);
       $result =  $query->result();
